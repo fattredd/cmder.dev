2d243fccad5a0353003dd0b3a7e8a784384551e5
JButler <fattredd@gmail.com>
2020-10-19 13:22:20 -0400

    Modify to work with my settings

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,13 +1,21 @@
-﻿$packageName = 'battecode.cmder.dev'
-$url = 'https://github.com/jbatte47/cmder.dev/releases/download/1.1.3.1-zeta/cmder.7z'
+﻿$packageName = 'fattredd.cmder.dev'
+$url = 'https://github.com/cmderdev/cmder/releases/download/v1.3.16/cmder.7z'
 
 try { 
-  $installDir = "$(Join-Path $env:USERPROFILE AppData\Local\cmder)" 
+  $installDir = "C:\bin" 
   
   if (![System.IO.Directory]::Exists($installDir)) {[System.IO.Directory]::CreateDirectory($installDir)}
   
   $tempDir = "$env:TEMP\chocolatey\$($packageName)"
   if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir)}
+
+  # Get the latest Cmder 7z file
+  $repo = "cmderdev/cmder"
+  $file = "cmder.7z"
+  $releases = "https://api.github.com/repos/$repo/releases"
+  Write-Host Determining latest release
+  $tag = (Invoke-WebRequest $releases -UseBasicParsing | ConvertFrom-Json)[0].tag_name
+  $url = "https://github.com/$repo/releases/download/$tag/$file"
 
   $file = Join-Path $tempDir "$($packageName).7z"
   Get-ChocolateyWebFile "$packageName" "$file" "$url"
@@ -19,3 +27,4 @@ try {
   Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
   throw 
 }
+#>
